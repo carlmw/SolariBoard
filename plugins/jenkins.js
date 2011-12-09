4d5c4f6b85210c77e06f4335e4050a021d86cc38
Carl Whittaker <carl.whittaker@hogarthww.com>
2011-12-09 13:31:51 +0000

    Adding a little logic to reconnect the websocket periodically

--- plugins/jenkins.js
+++ plugins/jenkins.js
@@ -2,8 +2,7 @@
 	const HOST = 'dev-hson-1';
 	const ZONZAHOST = 'dev-jen1';
 	const CHARCOUNT = 20;
-	var ws = new WebSocket('ws://' + HOST + ':8082/jenkins'),
-	    ws2 = new WebSocket('ws://' + ZONZAHOST + ':8081/jenkins'),
+	var ws, ws2,
 		output = JSON.parse(localStorage.getItem('messages'))||[],
 		lines = {
 			'fidodevelopment': 'FIDODEV',
@@ -29,11 +28,23 @@
 			localStorage.setItem('messages', JSON.stringify(output));
 		};
 	
-	ws.onmessage = ws2.onmessage = function(msg) {
-		var data = JSON.parse(msg.data);
-		
-		render(data);
+	var connect = function(){
+	    
+	    if(ws) ws.close();
+	    if(ws2) ws2.close();
+	    
+	    ws = new WebSocket('ws://' + HOST + ':8082/jenkins'),
+    	ws2 = new WebSocket('ws://' + ZONZAHOST + ':8081/jenkins');
+    	    
+        ws.onmessage = ws2.onmessage = function(msg) {
+    		var data = JSON.parse(msg.data);
+
+    		render(data);
+    	};
 	};
+	
+	setInterval(connect, 5000);
+    connect();
 	
 	Board.bind('start', function(){
 		Board.setMessage(output);
