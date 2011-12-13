d0eadc0f114832bf4ec44ab458afc74f469765ba
Greg Furga <greg.furga@hogarthww.com>
2011-12-13 16:10:40 +0000

    Fixing flaps

--- src/SolariFlap.js
+++ src/SolariFlap.js
@@ -4,28 +4,28 @@ var DEG2RAD =  Math.PI / 180,
 var SolariFlap = Backbone.View.extend({
     MAX_X: 180 * DEG2RAD,
 
-	initialize: function(textureSet, x, y){
-		var flapWidth = textureSet.faceWidth,
-			flapHeight = textureSet.faceHeight,
-			top = new THREE.Mesh(
-				new THREE.PlaneGeometry(flapWidth, flapHeight),
-				textureSet.spriteMaterial),
-			bottom = new THREE.Mesh(
-				new THREE.PlaneGeometry(flapWidth, flapHeight),
-				textureSet.spriteMaterial),
-			flap = new THREE.Mesh(
-				new THREE.CubeGeometry(flapWidth, flapHeight, 0, 1, 1, 1, [
+    initialize: function(textureSet, x, y){
+        var flapWidth = textureSet.faceWidth,
+            flapHeight = textureSet.faceHeight,
+            top = new THREE.Mesh(
+                new THREE.PlaneGeometry(flapWidth, flapHeight),
+                textureSet.spriteMaterial),
+            bottom = new THREE.Mesh(
+                new THREE.PlaneGeometry(flapWidth, flapHeight),
+                textureSet.spriteMaterial),
+            flap = new THREE.Mesh(
+                new THREE.CubeGeometry(flapWidth, flapHeight, 0, 1, 1, 1, [
                     null, null, null, null,
                     textureSet.spriteMaterial,
                     textureSet.spriteMaterial
                 ]),
-				new THREE.MeshFaceMaterial()),
+                new THREE.MeshFaceMaterial()),
             varia = 1.1 - Math.random() * 0.2;
 
         this.SPEED = SPEED * DEG2RAD / 1000.0 * varia;
 
 
-		this.textureSet = textureSet;
+        this.textureSet = textureSet;
         this.top_g = top.geometry;
         this.bottom_g = bottom.geometry;
         this.flap_g = flap.geometry;
@@ -41,9 +41,9 @@ var SolariFlap = Backbone.View.extend({
 
         this.objToRender = [top, bottom, this.flapWrapper];
 
-		this.i = 0;
-        this.setUpTextures(textureSet.max, 0);
-	},
+        this.i = 0;
+        this.setUpTextures(0, 1);
+    },
     setUpTextures: function(from, to) {
         /* Setting up the coming character. */
         var current = this.textureSet.UV[from],
@@ -57,24 +57,25 @@ var SolariFlap = Backbone.View.extend({
         this.top_g.__dirtyUvs = this.bottom_g.__dirtyUvs = this.flap_g.__dirtyUvs = true;
     },
 
-	setChar: function(ch){
-		var i = this.textureSet.chars.indexOf(ch);
-		this.currentChar = i != -1 ? i : this.textureSet.max;
-		return this;
-	},
-	next: function(){
-		if(this.currentChar === this.i){
-			this.wedged = true;
-		}else{
-			this.wedged = false;
+    setChar: function(ch){
+        var i = this.textureSet.chars.indexOf(ch);
+        this.currentChar = i != -1 ? i : this.textureSet.max;
+        this.wedged = this.currentChar == this.i;
+        return this;
+    },
+    next: function(){
+        this.i = this.i >= this.textureSet.max ? 0 : this.i + 1;
+        var next = (this.i+1>this.textureSet.max) ? 0 : this.i + 1;
+        this.setUpTextures(this.i, next);
 
-            var prev = this.i;
-			this.i = this.i >= this.textureSet.max ? 0 : this.i + 1;
-            this.setUpTextures(prev, this.i);
-		}
-	},
+        if(this.currentChar === this.i){
+            this.wedged = true;
+        }else{
+            this.wedged = false;
+        }
+    },
     update: function(diff) {
-        x = this.flapWrapper.rotation.x;
+        var x = this.flapWrapper.rotation.x;
         if (this.wedged) return;
 
         x += diff * this.SPEED;
