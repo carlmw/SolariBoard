fcdbcc0a20afb0952e412e5e22aed1cb362796bd
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-12-19 16:17:14 +0000

    Fixing up dodgy commit for adding persistence.

--- plugins/jenkins.js
+++ plugins/jenkins.js
@@ -15,7 +15,7 @@ JenkinsPlugin = _.extend({
         SolariPlugin.prototype.init.call(this, scr);
         if(this.ws) this.ws.close();
         if(this.ws2) this.ws2.close();
-        this.ws = new WebSocket('ws://dev-hson-1:8082/jenkins');
+        this.ws = new WebSocket('ws://mob061.lan:8081/jenkins');
         this.ws2 = new WebSocket('ws://dev-jen1:8081/jenkins');
     },
     start: function(){
@@ -36,14 +36,9 @@ JenkinsPlugin = _.extend({
                 });
             }
         }, this);
-        this.updateScreen();
         var render = function(data){
             var row = self.rowMap[data.project],
                 buildNo = '#' + data.number;
-            row[row.length - 1] = data.result[0];
-            for(i = 0; i < buildNo.length; i++) {
-                var c = (row.length - 1) - (buildNo.length - i);
-                row[c] = buildNo[i];
             if(row){
                 row[row.length - 1] = data.result[0];
                 for(i = 0; i < buildNo.length; i++) {
@@ -53,13 +48,13 @@ JenkinsPlugin = _.extend({
                 self.updateScreen();
             }
         };
-        this.ws.onmessage = this.ws2.onmessage = function(msg) {
         _.each(keys, function(key){
             if (storedMessage = JSON.parse(localStorage.getItem(key))){
                 render(storedMessage);
             }
         }, this);
         this.updateScreen();
+        this.ws.onmessage = this.ws2.onmessage = function(msg) {
             var data = JSON.parse(msg.data);
             localStorage.setItem(data.project, msg.data);
             render(data);
