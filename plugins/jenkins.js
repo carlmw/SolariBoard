a73173fb409a05a0395fb0a6c5d7cb82dbf2754d
Carl Whittaker <carl359@gmail.com>
2011-12-19 20:32:02 +0000

    Genericising

--- plugins/jenkins.js
+++ plugins/jenkins.js
@@ -1,21 +1,10 @@
 (function(){
-	const HOST = 'dev-hson-1';
-	const ZONZAHOST = 'dev-jen1';
+	const WSHOST = 'websockethost:port';
 	const CHARCOUNT = 20;
 	var ws, ws2,
 		output = JSON.parse(localStorage.getItem('messages'))||[],
 		lines = {
-			'fidodevelopment': 'FIDODEV',
-			'fidotesting': 'FIDOTEST',
-			'kerby-server': 'KERBYSVR',
-			'kerby-fido-integration': 'KERBYFIDOINT',
-			'Zonza': 'ZONZA',
-			'ZonzaRest': 'ZONZAREST',
-			'ZonzaSelenium': 'ZONZASELENIUM',
-			'Skellington': 'SKELLINGTON',
-			'Selenium_Tests': 'FIDOSELENIUM',
-			'kerby-ui': 'KERBYUI',
-			'fidotestingdeployment': 'FIDOTESTDEPLOY'
+			'buildname': 'BUILDALIAS'
 		},
 		render = function(data){
 			if(!lines[data.project]) return;
@@ -35,12 +24,10 @@
 	var WebSocket = WebSocket||MozWebSocket,
 	    connect = function(){
 	    if(ws) ws.close();
-	    if(ws2) ws2.close();
 	    
-	    ws = new WebSocket('ws://' + HOST + ':8082/jenkins'),
-    	ws2 = new WebSocket('ws://' + ZONZAHOST + ':8081/jenkins');
+	    ws = new WebSocket('ws://' + HOST + '/jenkins'),
     	    
-        ws.onmessage = ws2.onmessage = function(msg) {
+        ws.onmessage = function(msg) {
     		var data = JSON.parse(msg.data);
 
     		render(data);
@@ -48,7 +35,6 @@
     	
     	setInterval(function(){
     	    ws.send('ping');
-    	    ws2.send('pong');
     	}, 5000);
 	};
 	
