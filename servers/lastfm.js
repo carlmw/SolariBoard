804e983df1c0bd0b90958a88afcd6265cf73c029
Kevin Morgan <kevin.morgan@hogarthww.com>
2011-12-24 21:14:43 +0000

    Adding back in last.fm node server.

---
+++ servers/lastfm.js
@@ -0,0 +1,46 @@
+var LastFmNode = require('lastfm').LastFmNode;
+var http = require('http');
+var io = require('socket.io').listen(8090);
+
+var sockets = [];
+
+io.sockets.on('connection', function(socket){
+    sockets.push(socket);
+});
+
+var lastfm = new LastFmNode({
+    api_key: process.env.LAST_FM_API_KEY,
+    secret: process.env.LAST_FM_SECRET
+});
+
+var users = ['kevbear', 'carl359'],
+    streams = [];
+
+for (i = 0; i < users.length; i++) {
+    streams[i] = lastfm.stream(users[i]);
+    function createListener(username) {
+        return function(track) {
+            for (a = 0; a < sockets.length; a++) {
+                sockets[a].emit(
+                    'lastfm',
+                    {
+                        username: username,
+                        track: track.name,
+                        artist: track.artist['#text']
+                    }
+                )
+            }
+        }
+    }
+    streams[i].on(
+        'nowPlaying',
+        createListener(users[i])
+    );
+    streams[i].on(
+        'error',
+        function(error) {
+            //NOOP
+        }
+    );
+    streams[i].start();
+}
