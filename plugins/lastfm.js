35ae8649cea2f897e72873895bc31c5d5aeed2ee
Kevin Morgan <kevin.morgan@hogarthww.com>
2012-02-11 21:03:32 +0000

    Changing how the plugins are defined.

--- plugins/lastfm.js
+++ plugins/lastfm.js
@@ -1,17 +1,20 @@
 LastFmPlugin = function(socket_url){
-    this.init = function(scr){
-        SolariPlugin.prototype.init.call(this, scr);
-        this.socket = new io.connect(socket_url);
-    };
-    this.start = function(){
-        this.updateScreen();
-        var self = this;
-        this.socket.on('lastfm', function(track){
-            self.scr.pushMessage(track.track.toUpperCase());
-            self.scr.pushMessage(track.artist.toUpperCase());
-            self.updateScreen();
-        });
-    };
+    this.socket_url = socket_url;
 };
 
 _.extend(LastFmPlugin.prototype, SolariPlugin.prototype);
+
+LastFmPlugin.prototype.init = function(scr){
+    SolariPlugin.prototype.init.call(this, scr);
+    this.socket = new io.connect(this.socket_url);
+};
+
+LastFmPlugin.prototype.start = function(){
+    this.updateScreen();
+    var self = this;
+    this.socket.on('lastfm', function(track){
+        self.scr.pushMessage(track.track.toUpperCase());
+        self.scr.pushMessage(track.artist.toUpperCase());
+        self.updateScreen();
+    });
+};
--- plugins/time.js
+++ plugins/time.js
@@ -1,30 +1,31 @@
-TimePlugin = function(){
-    this.init = function(scr) {
-        SolariPlugin.prototype.init.call(this, scr);
-        this.worker = new Worker('plugins/time-worker.js');
-        var self = this;
-        this.worker.addEventListener(
-            'message',
-            function(e) {
-                function pad(number) {
-                    return ((number < 10) ? '0': '') + number;
-                }
-                var rowNum = Math.ceil(self.scr.matrix.length / 2) - 1,
-                    row = self.scr.matrix[rowNum],
-                    str = pad(e.data.hour) + '.' + pad(e.data.minutes);
-                    rowStart = Math.floor((row.length - str.length) / 2);
-                for (i = 0; i < str.length; i++) {
-                    row[rowStart + i] = str[i];
-                }
-                self.updateScreen();
-            },
-            false
-        );
-    };
-    this.start = function() {
-        this.worker.postMessage();
-        this.updateScreen();
-    };
-};
+TimePlugin = function(){};
 
 _.extend(TimePlugin.prototype, SolariPlugin.prototype);
+
+TimePlugin.prototype.init = function(scr) {
+    SolariPlugin.prototype.init.call(this, scr);
+    this.worker = new Worker('plugins/time-worker.js');
+    var self = this;
+    this.worker.addEventListener(
+        'message',
+        function(e) {
+            function pad(number) {
+                return ((number < 10) ? '0': '') + number;
+            }
+            var rowNum = Math.ceil(self.scr.matrix.length / 2) - 1,
+                row = self.scr.matrix[rowNum],
+                str = pad(e.data.hour) + '.' + pad(e.data.minutes);
+                rowStart = Math.floor((row.length - str.length) / 2);
+            for (i = 0; i < str.length; i++) {
+                row[rowStart + i] = str[i];
+            }
+            self.updateScreen();
+        },
+        false
+    );
+};
+
+TimePlugin.prototype.start = function() {
+    this.worker.postMessage();
+    this.updateScreen();
+};
