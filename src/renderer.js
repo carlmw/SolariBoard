4980e284ee042a8cc3aad42a248a89350f96e82b
Grzes Furga <grzesiof@gmail.com>
2012-08-13 11:57:41 +0200

    Adding a flat color background

--- src/renderer.js
+++ src/renderer.js
@@ -167,7 +167,7 @@ define([
         gl.uniformMatrix4fv(shader.uniform.projectionMat, false, this.perspectiveMat);
 
         gl.bindFramebuffer(gl.FRAMEBUFFER, this.renderBuffer.id);
-        gl.clearColor(0, 0, 0, 1);
+        gl.clearColor(0.1, 0.1, 0.1, 1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.uniform1f(shader.uniform.timing, board.timing);
